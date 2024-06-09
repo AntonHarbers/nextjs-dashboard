e0c7ffe03b2d98e5427cd518cf5ccb06153d23a8
Cosmicode <anton.harbers23@gmail.com>
2024-06-09 14:05:26 +0200

    FEAT: courses chapter 8-11, partial prerendering with suspense and loading file, route grouping with (), data fetching and pagination with searchParams

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -39,6 +39,8 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -64,6 +66,10 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
